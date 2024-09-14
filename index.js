f3f588abab5d32a18b292f11bdaffe6171fcce18
Raghib Mirza <realraghibmrz@gmail.com>
2024-09-14 21:46:50 +0100

    committing all code for backend challenge 1

--- index.js
+++ index.js
@@ -19,11 +19,20 @@ app.get("/", function (req, res) {
 });
 
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
-});
+app.get("/api/:date?", function (req, res) {
+  let date = req.params.date;
+  let isnum = /^\d+$/.test(date);
+  if (isnum) {
+    date = parseInt(date);
+  }
 
+  let parsed_date = date ? new Date(date) : new Date();
+  if (parsed_date.toString() === "Invalid Date") {
+    res.json({ error: parsed_date.toString() });
+  } else {
+    res.json({ unix: parsed_date.getTime(), utc: parsed_date.toUTCString() });
+  }
+});
 
 
 // Listen on port set in environment variable or default to 3000
